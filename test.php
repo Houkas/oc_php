b3176118d04cb5e68a5efc9636d478dfd581c7a9
Houkas <hugor33@outlook.fr>
2021-01-07 22:06:35 +0100

    ajout du fichier test.php

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OC PHP MYSQL</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+
+        </header>
+
+        <h1>Cours Openclassrooms : Concevoir un site en PHP et MYSQL</h1>
+        <p><?php echo "Hello World"; ?></p>
+
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
