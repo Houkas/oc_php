ff194415d3a8403a207891e95254d2411eea2136
Houkas <hugor33@outlook.fr>
2021-01-08 15:09:14 +0100

    commit lecon avant variables

--- test.php
+++ test.php
@@ -10,7 +10,7 @@
         </header>
 
         <h1>Cours Openclassrooms : Concevoir un site en PHP et MYSQL</h1>
-        <p><?php echo "Hello World"; ?></p>
+        <p><?php echo "Hello World"; phpinfo() ?></p>
 
 
         <footer>
